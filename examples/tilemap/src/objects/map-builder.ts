1107435f1d1e5d487933cd24dae7f9fd23c1f981
tenpaMk2 <wavyroom@gmail.com>
2022-03-30 20:23:17 +0900

    `tilemap` : add breakable tree.

--- examples/tilemap/src/objects/map-builder.ts
+++ examples/tilemap/src/objects/map-builder.ts
@@ -1,10 +1,14 @@
-import { Engine, SpriteSheet, TileMap } from "excalibur";
+import { Engine, Sprite, SpriteSheet, TileMap, Vector } from "excalibur";
 import { Resources } from "../resource";
 
 export class MapBuilder extends TileMap {
   mapchipSpriteSheet: SpriteSheet;
+  blockSprite: Sprite;
+  grasslandSprite: Sprite;
+  upTreeSprite: Sprite;
+  downTreeSprite: Sprite;
 
-  constructor(unitLength: number) {
+  constructor(public unitLength: number) {
     super({
       x: 0,
       y: 0,
@@ -30,6 +34,19 @@ export class MapBuilder extends TileMap {
       },
     });
 
+    this.blockSprite = this.mapchipSpriteSheet.getSprite(6, 0);
+    this.blockSprite.width = unitLength;
+    this.blockSprite.height = unitLength;
+    this.grasslandSprite = this.mapchipSpriteSheet.getSprite(5, 0);
+    this.grasslandSprite.width = unitLength;
+    this.grasslandSprite.height = unitLength;
+    this.upTreeSprite = this.mapchipSpriteSheet.getSprite(16, 10);
+    this.upTreeSprite.width = unitLength;
+    this.upTreeSprite.height = unitLength;
+    this.downTreeSprite = this.mapchipSpriteSheet.getSprite(16, 11);
+    this.downTreeSprite.width = unitLength;
+    this.downTreeSprite.height = unitLength;
+
     for (let row = 0; row < this.rows; row++) {
       for (let col = 0; col < this.cols; col++) {
         if (
@@ -44,25 +61,50 @@ export class MapBuilder extends TileMap {
         }
       }
     }
+
+    this.buildTree(3, 4);
+    this.buildTree(4, 3);
+    this.buildTree(4, 4);
   }
 
   onInitialize = (engine: Engine) => {};
 
   buildBlock = (row: number, col: number) => {
     const cell = this.getCell(col, row);
-    const sprite = this.mapchipSpriteSheet.getSprite(6, 0);
     cell.data.set("isBlocked", true);
-    sprite.width = cell.width;
-    sprite.height = cell.height;
-    cell.addGraphic(sprite);
+    cell.addGraphic(this.blockSprite);
   };
 
   buildGrassland = (row: number, col: number) => {
     const cell = this.getCell(col, row);
-    const sprite = this.mapchipSpriteSheet.getSprite(5, 0);
     cell.data.set("isBlocked", false);
-    sprite.width = cell.width;
-    sprite.height = cell.height;
-    cell.addGraphic(sprite);
+    cell.addGraphic(this.grasslandSprite);
+  };
+
+  buildTree = (row: number, col: number) => {
+    const upCell = this.getCell(col, row);
+    upCell.data.set("isBreakable", true);
+    upCell.addTag("tree");
+    upCell.addGraphic(this.downTreeSprite);
+
+    const downCell = this.getCell(col, row - 1);
+    downCell.addGraphic(this.upTreeSprite);
+  };
+
+  isBreakable = (targetPos: Vector) => {
+    const targetCell = this.getCellByPoint(targetPos.x, targetPos.y);
+    return targetCell.data.get("isBreakable");
+  };
+
+  breakdown = (targetPos: Vector) => {
+    const targetCell = this.getCellByPoint(targetPos.x, targetPos.y);
+    targetCell.removeGraphic(this.downTreeSprite);
+    if (targetCell.hasTag("tree")) {
+      const upCell = this.getCellByPoint(
+        targetPos.x,
+        targetPos.y - this.unitLength
+      );
+      upCell.removeGraphic(this.upTreeSprite);
+    }
   };
 }
--- examples/tilemap/src/scenes/game-scene.ts
+++ examples/tilemap/src/scenes/game-scene.ts
@@ -1,9 +1,10 @@
-import { Scene, Engine, TileMap, Vector } from "excalibur";
+import { Scene, Engine, Vector } from "excalibur";
 import { PointerEvent } from "excalibur/build/dist/Input";
 import { MapBuilder } from "../objects/map-builder";
 import { Player } from "../objects/player";
 
 export class GameScene extends Scene {
+  tilemap: MapBuilder;
   static readonly UNIT_LENGTH = 64;
 
   constructor(public engine: Engine) {
@@ -11,52 +12,65 @@ export class GameScene extends Scene {
   }
 
   onInitialize = (engine: Engine) => {
-    const tilemap = new MapBuilder(GameScene.UNIT_LENGTH);
-    engine.add(tilemap);
+    this.tilemap = new MapBuilder(GameScene.UNIT_LENGTH);
+    engine.add(this.tilemap);
 
-    const cell = tilemap.getCell(2, 3);
+    const cell = this.tilemap.getCell(2, 3);
     const player = new Player(cell.center, GameScene.UNIT_LENGTH);
     engine.add(player);
 
     engine.input.pointers.primary.on("down", (event: PointerEvent) => {
       if (event.screenPos.x < engine.drawWidth / 4) {
-        this.tryToMoveLeft(tilemap, player);
+        this.tryToMoveLeft(this.tilemap, player);
       } else if ((engine.drawWidth * 3) / 4 < event.screenPos.x) {
-        this.tryToMoveRight(tilemap, player);
+        this.tryToMoveRight(this.tilemap, player);
       }
 
       if (event.screenPos.y < engine.drawHeight / 4) {
-        this.tryToMoveUp(tilemap, player);
+        this.tryToMoveUp(this.tilemap, player);
       } else if ((engine.drawHeight * 3) / 4 < event.screenPos.y) {
-        this.tryToMoveDown(tilemap, player);
+        this.tryToMoveDown(this.tilemap, player);
       }
     });
 
     this.camera.strategy.elasticToActor(player, 0.2, 0.1);
   };
 
-  tryToMoveUp = (tilemap: TileMap, player: Player) => {
-    this.tryToMove(tilemap, player, Vector.Up);
+  tryToMoveUp = (mapBuilder: MapBuilder, player: Player) => {
+    this.tryToMove(mapBuilder, player, Vector.Up);
   };
 
-  tryToMoveRight = (tilemap: TileMap, player: Player) => {
-    this.tryToMove(tilemap, player, Vector.Right);
+  tryToMoveRight = (mapBuilder: MapBuilder, player: Player) => {
+    this.tryToMove(mapBuilder, player, Vector.Right);
   };
 
-  tryToMoveDown = (tilemap: TileMap, player: Player) => {
-    this.tryToMove(tilemap, player, Vector.Down);
+  tryToMoveDown = (mapBuilder: MapBuilder, player: Player) => {
+    this.tryToMove(mapBuilder, player, Vector.Down);
   };
 
-  tryToMoveLeft = (tilemap: TileMap, player: Player) => {
-    this.tryToMove(tilemap, player, Vector.Left);
+  tryToMoveLeft = (mapBuilder: MapBuilder, player: Player) => {
+    this.tryToMove(mapBuilder, player, Vector.Left);
   };
 
-  tryToMove = (tilemap: TileMap, player: Player, directionVector: Vector) => {
+  tryToMove = (
+    mapBuilder: MapBuilder,
+    player: Player,
+    directionVector: Vector
+  ) => {
     const pos = player.pos;
     const targetPos = pos.add(directionVector.scale(GameScene.UNIT_LENGTH));
-    const targetCell = tilemap.getCellByPoint(targetPos.x, targetPos.y);
+
+    this.breakIfNeed(mapBuilder, targetPos);
+
+    const targetCell = mapBuilder.getCellByPoint(targetPos.x, targetPos.y);
     const isBlocked = targetCell.data.get("isBlocked");
     if (isBlocked) return;
     player.pos = targetPos;
+  };
+
+  breakIfNeed = (mapBuilder: MapBuilder, targetPos: Vector) => {
+    const isBreakable = mapBuilder.isBreakable(targetPos);
+    if (!isBreakable) return;
+    this.tilemap.breakdown(targetPos);
   };
 }
